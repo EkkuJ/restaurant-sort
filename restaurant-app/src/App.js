ccc0d1e283d4bcc94b06cb0bb017960bb7740742
Ekku Jokinen <ekku.jokinen@aalto.fi>
2020-01-31 11:30:05 +0200

    Add json-loop and render data

--- restaurant-app/src/App.js
+++ restaurant-app/src/App.js
@@ -2,9 +2,25 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  // Fetch data from the local restaurants.json-file.
+  var jsonData = require('./restaurants.json');
+
+  var restaurants = [];
+
+  // Loop trough the JSON-Array and 
+  for (var i = 0; i < jsonData.restaurants.length; i++) {
+    var restaurant = jsonData.restaurants[i];
+    restaurants.push(restaurant)
+  }
+
   return (
     <div className="App">
-      
+      <>
+        {restaurants.map(restaurant => (
+          <div className="restaurant" key={restaurant.name}>{restaurant.name}</div>
+        ))}
+      </>
     </div>
   );
 }
