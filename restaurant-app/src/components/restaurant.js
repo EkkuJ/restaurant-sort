968e328d97d641dae822c0348a7aaac6eab2b0c7
Ekku Jokinen <ekku.jokinen@aalto.fi>
2020-01-31 14:40:10 +0200

    Clean up the restaurant-component

--- restaurant-app/src/components/restaurant.js
+++ restaurant-app/src/components/restaurant.js
@@ -3,6 +3,11 @@ import { Blurhash } from "react-blurhash";
 
 function Restaurant(props) {
 
+    /**
+     *  Element for focused restaurants. Contains the name, the picture, the description and the city
+     *  of the restaurant.
+     * */ 
+
     const sharp =
         <div className="sharpContainer">
             <p className="name">
@@ -20,7 +25,10 @@ function Restaurant(props) {
 
         </div>
 
-
+    /**
+     * Element for blurred restaurants. Contains the name of the restaurant on top of the blurred image of the restaurant.
+     * The blurred image is generated with react-blurhash by woltapp. For more details https://github.com/woltapp/react-blurhash.
+     */
     const blur =
         <div className="blurredContainer">
             <Blurhash
@@ -33,20 +41,13 @@ function Restaurant(props) {
             </p>
         </div>
 
-    const [image, setImage] = useState(blur);
-
-    function handleMouseEnter() {
-        setImage(sharp)
-    }
-
-    function handleMouseLeave() {
-        setImage(blur)
-    }
+    // Current status of the restaurant. Is either 'sharp' or 'blur'.
+    const [element, setElement] = useState(blur);
 
     return (
-        <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        <div onMouseEnter={setElement(sharp)} onMouseLeave={setElement(blur)}>
             <div className="restaurantContainer">
-                {image}
+                {element}
             </div>
         </div>
     );
