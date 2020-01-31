a5a1868e84515a27fdee20e945e3600d31b016fb
Ekku Jokinen <ekku.jokinen@aalto.fi>
2020-01-31 13:53:20 +0200

    Add more props to restaurants

--- restaurant-app/src/components/restaurant.js
+++ restaurant-app/src/components/restaurant.js
@@ -1,18 +1,49 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { Blurhash } from "react-blurhash";
 
-class Restaurant extends Component {
+function Restaurant(props) {
 
-    render() {
-        return (
-            <div>
-                <img className="Image"
-                    src={this.props.image}
-                    alt={this.props.name}
-                />
-            </div>
-        );
-        
+    const sharp = 
+    <div className="sharpContainer">
+        <span className="restaurantName">
+            {props.name}
+        </span>
+        <img className="sharpImage"
+        src={props.image}
+        alt="Image of food"/>
+        <p className="description">
+            {props.description}
+        </p>
+        <p className="city">
+            @ {props.city}
+        </p>
+
+    </div>
+
+
+    const blur = <Blurhash className="blurContainer"
+        hash={props.hash} 
+        width={300}
+        height={300}
+        />
+
+    const [image, setImage] = useState(blur);
+
+    function handleMouseEnter() {
+        setImage(sharp)
     }
+
+    function handleMouseLeave() {
+        setImage(blur)
+    }
+
+    return (
+        <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            <div className="restaurantContainer">
+                {image}
+            </div>
+        </div>
+    );
 }
 
 export default Restaurant;
