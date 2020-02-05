78dab64f1c9a5147aea9c2ab34f9907a0aaebefe
Ekku Jokinen <ekku.jokinen@aalto.fi>
2020-02-05 14:59:49 +0200

    Add sorting with hooks

--- restaurant-app/src/App.js
+++ restaurant-app/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
-import Restaurant from './components/Restaurant';
+import Restaurants from './components/Restaurants';
 
 function App() {
 
@@ -15,20 +15,48 @@ function App() {
     restaurants.push(restaurant)
   };
 
+  // The initial Restaurants-element based on the order in the JSON-file
+  const initialElement = <Restaurants restaurants={restaurants} />
+
+  // The state that contains the Restaurants-element in the wanted order (initial, A-Z or Z-A).
+  const [element, setElement] = useState(initialElement)
+
+  /**
+   * Create the A-Z and Z-A elements once and store them in ref-containers.
+   * The effect runs after the initial render.
+  */
+  const refAZ = useRef(initialElement)
+  const refZA = useRef(initialElement)
+  useEffect(() => {
+    function sortRestaurants() {
+      const restaurantsAZ = restaurants.slice().sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
+      const restaurantsZA = restaurants.slice().sort((a, b) => (a.name < b.name) ? 1 : ((b.name < a.name) ? -1 : 0));
+      refAZ.current = <Restaurants restaurants={restaurantsAZ} />
+      refZA.current = <Restaurants restaurants={restaurantsZA} />
+    }
+    sortRestaurants()
+  }, [restaurants])
+
+  // Functions for handling sort-button clicks.
+  function handleSortZA() {
+    setElement(refZA.current)
+  }
+  function handleSortAZ() {
+    setElement(refAZ.current)
+  }
+
   return (
     <div className="App" >
-      <>
-        {restaurants.map(restaurant => (
-          <Restaurant className="restaurant"
-          key={restaurant.name} 
-          name={restaurant.name}
-          image={restaurant.image}
-          hash={restaurant.blurhash}
-          description={restaurant.description}
-          city={restaurant.city}
-          />
-        ))}
-      </>
+      <header>
+        <h1 className="title">Restaurants</h1>
+        {(element === initialElement || element === refZA.current) ?
+          <button className="sortbtn" onClick={handleSortAZ}>Sort Ascending</button> :
+          <button className="sortbtn" onClick={handleSortZA}>Sort Descending </button>
+        }
+      </header>
+      <div className="restataurantsContainer">
+        {element}
+      </div>
     </div>
   );
 }
