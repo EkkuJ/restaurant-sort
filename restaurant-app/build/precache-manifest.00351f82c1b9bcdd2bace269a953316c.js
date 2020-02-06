5e0f4ac38cc18acf00ae3739cc00cd46c303c809
Ekku Jokinen <ekku.jokinen@aalto.fi>
2020-02-06 18:20:10 +0200

    Build App

---
+++ restaurant-app/build/precache-manifest.00351f82c1b9bcdd2bace269a953316c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c99da2099914ab95e0b2ad1ef207c1fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "290626f23fa5e81167db",
+    "url": "/static/css/main.644a84ea.chunk.css"
+  },
+  {
+    "revision": "a1442e71b53b3b5ca0b6",
+    "url": "/static/js/2.b005294a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.b005294a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "290626f23fa5e81167db",
+    "url": "/static/js/main.eb307e18.chunk.js"
+  },
+  {
+    "revision": "2af9052b31ecaa8bec9c",
+    "url": "/static/js/runtime-main.f3148808.js"
+  }
+]);
